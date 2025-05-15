86bc57e6c1c4d9a6b6eeaefe494e967e2ba295b9
Oenothera <wjsj1015@naver.com>
2025-05-15 10:26:42 +0900

    Feat : 소개 페이지 추가, 로고 추가, react-three-fiber 패키지 추가
    1. 소개 페이지 추가
    - Introduction 페이지 추가
    - react-three-fiber를 활용한 webgl로 구현 예정
    
    2. 로고 추가
    - 로고 추가 (추후 디자인 수정가능)
    
    3.react-three-fiber 패키지 추가
    - 소개 페이지에 적용할 react-three-fiber 패키지 추가

---
+++ ssb-front/src/pages/Introduction/Introduction.tsx
@@ -0,0 +1,41 @@
+import * as Three from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { RefObject, useRef } from "react";
+
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function RotateBox() {
+  const meshRef = useRef<Three.Mesh>(null!);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+export default function Introduction() {
+  return (
+    <div>
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <RotateBox />
+      </Canvas>
+    </div>
+  );
+}
