f709dfb68cf52c83c802dfd5121dee4c7d0c02dc
Oenothera <wjsj1015@naver.com>
2025-05-19 13:53:46 +0900

    Feat : 스크롤 애니메이션 변경
    1. 스크롤 애니메이션 변경
    - fadeout 효과를 주기 위해서 materialRef를 통한 transparent, opacity 추가 적용
    - mesh position 세부 조정

--- ssb-front/src/pages/Introduction/Introduction.tsx
+++ ssb-front/src/pages/Introduction/Introduction.tsx
@@ -16,14 +16,19 @@ function Box() {
 function RotateBox({ position, startPoint, resetPoint }: BoxArgs) {
   const scroll = useScroll();
   const meshRef = useRef<Three.Mesh>(null!);
+  const materialRef = useRef<Three.Material>(null!);
 
   useFrame(() => {
+    console.log(materialRef.current.opacity);
+    materialRef.current.opacity = 1 - scroll.offset;
+    materialRef.current.transparent = true;
     if (meshRef.current) {
       if (scroll && scroll?.offset !== 0) {
         console.log(scroll.offset);
-        meshRef.current.rotation.x = scroll.offset;
+        //meshRef.current.rotation.x = scroll.offset;
         meshRef.current.rotation.y = scroll.offset;
       } else {
+        meshRef.current.visible = true;
         console.log(
           meshRef.current.position.x,
           resetPoint,
@@ -40,7 +45,7 @@ function RotateBox({ position, startPoint, resetPoint }: BoxArgs) {
   return (
     <mesh ref={meshRef} position={[position[0], position[1], position[2]]}>
       <boxGeometry args={[10, 50, 0.1]} />
-      <meshStandardMaterial color="white" />
+      <meshStandardMaterial ref={materialRef} color="white" />
     </mesh>
   );
 }
@@ -58,25 +63,39 @@ export default function Introduction() {
         <ScrollControls>
           <ambientLight intensity={0.5} />
           <pointLight position={[10, 10, 10]} />
-          <RotateBox position={[-60, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[-45, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[-30, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[-15, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[0, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[15, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[30, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[45, 0, 0]} startPoint={-60} resetPoint={60} />
-          <RotateBox position={[60, 0, 0]} startPoint={-60} resetPoint={60} />
+          <RotateBox position={[-75, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-60, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-45, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-30, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-15, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[0, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[15, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[30, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[45, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[60, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[75, 0, 0]} startPoint={-75} resetPoint={75} />
           <gridHelper args={[10, 10]} />
           <axesHelper args={[8]} />
         </ScrollControls>
       </Canvas>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-
-        <gridHelper args={[10, 10]} />
-        <axesHelper args={[8]} />
+      <Canvas camera={{ position: [0, 0, 10] }}>
+        <ScrollControls>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <RotateBox position={[-75, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-60, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-45, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-30, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[-15, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[0, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[15, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[30, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[45, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[60, 0, 0]} startPoint={-75} resetPoint={75} />
+          <RotateBox position={[75, 0, 0]} startPoint={-75} resetPoint={75} />
+          <gridHelper args={[10, 10]} />
+          <axesHelper args={[8]} />
+        </ScrollControls>
       </Canvas>
     </div>
   );
