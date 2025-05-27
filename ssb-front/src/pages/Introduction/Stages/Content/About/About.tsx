607baf61c639a75b529d06d97cc5650697ddbeb8
김원진 <76833065+oen0thera@users.noreply.github.com>
2025-05-27 18:57:55 +0900

    Feat : About Stage 파티클 sphere 및 rotate
    1. About Stage 파티클 sphere 및 rotate
    - About Stage에 파티클 sphere 및 rotate 구현을 통해 프로토타입 페이지 구현
    - 추후 기획에 따라 페이지 디자인 및 구현 방향 수정될 수 있음

--- ssb-front/src/pages/Introduction/Stages/Content/About/About.tsx
+++ ssb-front/src/pages/Introduction/Stages/Content/About/About.tsx
@@ -1,5 +1,41 @@
 import { AboutProps } from "@/types/components/pages/introduction/Content/About.type";
+import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+
+import styles from './about.module.scss';
 
 export default function About({scroll,nextStage}:AboutProps) {
-    return <div>About</div>
+    const BasicParticles = ()=>{
+        const points = useRef(null!);
+
+        return(
+            <points ref={points}>
+                <sphereGeometry args={[10,48,48]}/>
+                <pointsMaterial color="#5786F5" size={0.1} sizeAttenuation/>
+            </points>
+        )
+    }
+    return <div>
+        <section className={styles.canvas}>
+        <Canvas camera={{ position: [0, 1, -10] }}>
+            <ScrollControls pages={30} >
+                <Scroll html>
+                    <div></div>
+                </Scroll>
+                
+              <ambientLight intensity={2.5} />
+              <pointLight position={[10, 10, 10]} />
+              
+              <BasicParticles />
+              
+              <gridHelper args={[10, 10]} />
+              <axesHelper args={[8]} />
+              <OrbitControls autoRotate autoRotateSpeed={0.5}/>
+              
+            </ScrollControls>
+            
+          </Canvas>
+          </section>
+          </div>
 }
