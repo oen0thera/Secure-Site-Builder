046601bfed80c85b3957c83912d7126ae8871544
Oenothera <wjsj1015@naver.com>
2025-05-31 19:45:12 +0900

    Feat : 기획 변경에 따른 모듈화, 리팩토링 및 내부 요소 수정
    1. 기획 변경에 따른 모듈화, 리팩토링 진행
    - 기획 변경(Issue #30)에 따라 모듈화 및 리팩토링 진행
    - RightContent, LeftContent -> ShowcaseUnitRightContent, ShowcaseUnitLeftContent, 네이밍 변경 및 모듈화 진행
    - ScrollDetect 모듈화 진행
    - 내부에 정의한 type들을 Showcase.type.ts로 이전, ShowcaseUnitArgs로 네이밍 변경 및 ShowcaseUnitEnum 추가
    - 그외 모듈화 진행에 따라 코드를 분리 및 디렉토리를 추가함
    
    2. 내부 요소 수정
    - img 대신 video 요소를 좌측 컨텐트 내부요소로 사용
    - showcase_1.mp4를 public/video에 추가
    - ShowcaseUnit의 배경색을 #000000으로 수정

---
+++ ssb-front/src/pages/Introduction/Stages/Content/Showcase/ShowcaseUnit/ShowcaseLeftContent.tsx
@@ -0,0 +1,146 @@
+import * as Three from "three";
+import {
+  RoundedBox,
+  Text,
+  useScroll,
+  useVideoTexture,
+} from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { ShowcaseUnitArgs } from "@/types/components/pages/introduction/Content/Showcase.type";
+
+const ShowcaseLeftContent = ({ position, page }: ShowcaseUnitArgs) => {
+  const meshRef = useRef<Three.Mesh>(null!);
+  const materialRef = useRef<Three.Material>(null!);
+  //const texture = useLoader(Three.TextureLoader,'/images/sample_image.png')
+  const texture = useVideoTexture("/videos/showcase_1.mp4", {
+    loop: true,
+    start: false,
+    muted: true,
+    crossOrigin: "anonymous",
+  });
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const textRef = useRef<Three.Mesh>(null!);
+  const subTextRef = useRef<Three.Mesh>(null!);
+  const scroll = useScroll();
+
+  texture.wrapS = Three.ClampToEdgeWrapping;
+  texture.wrapT = Three.ClampToEdgeWrapping;
+  texture.center.set(0.5, 0.5);
+  texture.offset.set(0, 0);
+  texture.repeat.set(1, 1);
+  texture.rotation = 0;
+
+  const handlePointerOver = () => {
+    videoRef.current?.play();
+  };
+
+  const handlePointerOut = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+  };
+
+  if (texture.image && !videoRef.current) {
+    //videoRef에 texture 내 video 태그 지정
+    videoRef.current = texture.image as HTMLVideoElement;
+  }
+
+  useFrame((state) => {
+    materialRef.current.transparent = true;
+    const textMat = textRef.current.material as Three.Material;
+    const subTextMat = subTextRef.current.material as Three.Material;
+    textMat.transparent = true;
+    const camera = new Three.Vector3(0, 0, -10);
+    const cameraPos = new Three.Vector3(-state.pointer.x, state.pointer.y, 0);
+    const pointerPos = camera.lerp(cameraPos, 0.01);
+    state.camera.lookAt(pointerPos);
+
+    if (meshRef.current) {
+      let scrollPosition = 0;
+      meshRef.current.visible = true;
+      textRef.current.visible = true;
+
+      if (materialRef.current.opacity < 1) materialRef.current.opacity += 0.01;
+      const radius = 8;
+      const baseX = position[0];
+      const baseZ = position[2];
+
+      const angle = -scroll.offset * Math.PI;
+
+      //meshRef.current.position.x = baseX*1.5 - Math.cos(angle) * radius;
+      meshRef.current.position.z =
+        baseZ - Math.sin(angle) * radius - scroll.offset * scroll.pages * 5;
+      //textRef.current.position.x = baseX*1.5 - Math.cos(angle) * radius -18;
+
+      textRef.current.position.z =
+        baseZ - Math.sin(angle) * radius - scroll.offset * scroll.pages * 5;
+      //subTextRef.current.position.x = baseX - Math.cos(angle) * radius -18;
+
+      subTextRef.current.position.z =
+        baseZ - Math.sin(angle) * radius - scroll.offset * scroll.pages * 5;
+
+      const stageOffset = scroll.offset * 4; // 페이지 4단계 정규화
+      const fadeRange = 0.9; // 얼마나 일찍/늦게 보이게 할지
+      const fadeSpeed = 0.1;
+
+      const distance = Math.abs(stageOffset - page);
+      const shouldShow = distance < fadeRange;
+
+      const targetOpacity = shouldShow ? 1 : 0;
+
+      textMat.opacity += (targetOpacity - textMat.opacity) * fadeSpeed;
+      subTextMat.opacity += (targetOpacity - subTextMat.opacity) * fadeSpeed;
+    }
+  });
+  return (
+    <>
+      <mesh
+        ref={meshRef}
+        position={[position[0], position[1], position[2]]}
+        rotation={[0, 10, 0]}
+        onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
+      >
+        <RoundedBox args={[10, 15, 0.1]} radius={0.5}>
+          <meshStandardMaterial color="#000000" />
+        </RoundedBox>
+        <meshStandardMaterial
+          ref={materialRef}
+          map={texture}
+          color="black"
+          opacity={0}
+        />
+        <mesh position={[0, 0, 0.501]}>
+          {" "}
+          {/* 박스 바로 위에 올림 */}
+          <planeGeometry args={[9, 14]} />
+          <meshStandardMaterial map={texture} transparent />
+        </mesh>
+      </mesh>
+      <Text
+        ref={textRef}
+        fontSize={2}
+        position={[-10, 4, 20]}
+        rotation={[0, 8.9, 0]}
+        material-opacity={0}
+        material-depthWrite={true}
+      >
+        Why SSB?
+      </Text>
+      <Text
+        ref={subTextRef}
+        fontSize={1}
+        position={[-10, 1, 20]}
+        rotation={[0, 8.9, 0]}
+        material-opacity={0}
+        material-depthWrite={true}
+      >
+        {`We know what we do\nAs you do what you want`}
+      </Text>
+    </>
+  );
+};
+
+export default ShowcaseLeftContent;
