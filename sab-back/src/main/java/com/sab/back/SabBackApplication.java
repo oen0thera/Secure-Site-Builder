c826a25a41171ca9a7c3e15d64a6e717e04efb09
Oenothera <wjsj1015@naver.com>
2026-01-29 17:25:20 +0900

    feat : sab-back 백엔드 생성
    1. sab-back 백엔드 생성

---
+++ sab-back/src/main/java/com/sab/back/SabBackApplication.java
@@ -0,0 +1,13 @@
+package com.sab.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabBackApplication.class, args);
+	}
+
+}
---
+++ sab-back/src/test/java/com/sab/back/SabBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sab.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
