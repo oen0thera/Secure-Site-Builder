4a1ee92c0010c2f34c5bd6b27264c55f9b49a496
Oenothera <wjsj1015@naver.com>
2026-02-13 17:04:19 +0900

    feat : detail gnb input 구성 및 타입 구현
    1. detail gnb input 구성 및 타입 구현

--- sab-front/src/pages/Process/Detail/Detail.tsx
+++ sab-front/src/pages/Process/Detail/Detail.tsx
@@ -3,9 +3,21 @@ import styles from "./detail.module.scss";
 import DetailGNB from "@/pages/Process/Detail/DetailComponents/DetailGNB/DetailGNB";
 import { useState } from "react";
 import DetailContent from "@/pages/Process/Detail/DetailComponents/DetailContent/DetailContent";
+import DetailInputs from "@/pages/Process/Detail/DetailInputs/DetailInputs";
+import {
+  DETAIL_GNB_INPUTS,
+  DETAIL_POSITION,
+  DETAIL_TYPE,
+  DetailInputData,
+  DetailPosition,
+  DetailType,
+} from "@/types/components/pages/details/detail.type";
 
 export default function Detail() {
   const [selectedComponent, setSelectedComponent] = useState<string>("");
+  const [detailType, setDetailType] = useState<DetailType>();
+  const [detailPositions, setDetailPositions] = useState<DetailPosition>();
+  const [detailInputs, setDetailInputs] = useState<DetailInputData>();
   const { selectedComponentPositions } = useCustomStore();
 
   const isRow =
@@ -14,12 +26,42 @@ export default function Detail() {
 
   const handleSelected = (item: string) => {
     setSelectedComponent(item);
+    const selectedComponentElements = item.split("_");
+    const validatedType = Object.values(DETAIL_TYPE).find(
+      (t) => t === selectedComponentElements[1]
+    );
+    const validatedPosition =
+      selectedComponentElements[2] &&
+      Object.values(DETAIL_POSITION).find(
+        (p) => p === selectedComponentElements[2]
+      );
+    // ex) custom_gnb_top -> ['custom', 'gnb', 'top']
+    // type : selectedComponentElements[1], position : selectedComponentElements[2]
+    if (validatedType) {
+      //타입 설정
+      setDetailType(validatedType);
+    }
+    if (validatedPosition) {
+      //위치 설정
+      setDetailPositions(validatedPosition);
+    }
+    switch (validatedType) {
+      case "gnb":
+        setDetailInputs(DETAIL_GNB_INPUTS);
+        break;
+    }
   };
 
   return (
     <>
       <div className={styles.preview_wrapper}>
-        {selectedComponent && <div className={styles.detail_inputs}></div>}
+        {selectedComponent && (
+          <DetailInputs
+            type={detailType}
+            position={detailPositions}
+            inputs={detailInputs}
+          />
+        )}
         <div
           className={`${styles.preview} ${isRow ? styles.row : styles.column}`}
         >
---
+++ sab-front/src/pages/Process/Detail/DetailInputs/DetailInputs.tsx
@@ -0,0 +1,38 @@
+import {
+  DetailInputData,
+  DetailPosition,
+  DetailType,
+} from "@/types/components/pages/details/detail.type";
+import styles from "./detail_inputs.module.scss";
+
+interface DetailInputsProps {
+  type?: DetailType;
+  position?: DetailPosition;
+  inputs?: DetailInputData;
+}
+
+export default function DetailInputs({
+  type,
+  position,
+  inputs,
+}: DetailInputsProps) {
+  const renderInputs = () => {
+    switch (type) {
+      case "gnb":
+        return (
+          <div>
+            {inputs &&
+              Object.entries(inputs).map((item) => {
+                return (
+                  <div className={styles.input_container}>
+                    <div className={styles.input_label}>{item[0]}</div>
+                    <div className={styles.input_value}>{item[1]}</div>
+                  </div>
+                );
+              })}
+          </div>
+        );
+    }
+  };
+  return <div className={styles.detail_inputs}>{renderInputs()}</div>;
+}
