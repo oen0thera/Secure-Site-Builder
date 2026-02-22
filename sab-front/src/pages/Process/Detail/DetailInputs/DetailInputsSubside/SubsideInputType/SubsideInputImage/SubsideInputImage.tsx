97b6ee940202a73dc0b92abb703846607975bad3
Oenothera <wjsj1015@naver.com>
2026-02-22 15:22:17 +0900

    feat: 이미지 프리뷰 구현
    1. 이미지 프리뷰 구현

--- sab-front/src/pages/Process/Detail/DetailInputs/DetailInputsSubside/SubsideInputType/SubsideInputImage/SubsideInputImage.tsx
+++ sab-front/src/pages/Process/Detail/DetailInputs/DetailInputsSubside/SubsideInputType/SubsideInputImage/SubsideInputImage.tsx
@@ -5,15 +5,26 @@ import {
 import styles from "./subside_input_image.module.scss";
 import Icon from "@/components/Icon/Icon";
 import { IconColor, IconSize, IconSrc } from "@/types/components/Icon.type";
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 export default function SubsideInputImage({
   item,
 }: {
   item: DetailSubsideItem;
 }) {
+  const [imageUrl, setImageUrl] = useState<string>("");
   const { label, value } = item;
+
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileList: FileList | null = e.currentTarget.files;
+    if (fileList && fileList[0]) {
+      const file = fileList[0];
+      const imgUrl = URL.createObjectURL(file);
+      setImageUrl(imgUrl);
+    }
+  };
   return (
     <div className={styles.input_wrapper}>
       <h1>{DETAIL_INPUTS_DICT[label]}</h1>
@@ -23,16 +34,26 @@ export default function SubsideInputImage({
           inputRef.current?.click();
         }}
       >
-        <Icon
-          size={IconSize.SMALL}
-          src={IconSrc.IMAGE}
-          color={IconColor.DEFAULT}
-        />
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            style={{ width: "100%", height: "100%", objectFit: "fill" }}
+          />
+        ) : (
+          <Icon
+            size={IconSize.SMALL}
+            src={IconSrc.IMAGE}
+            color={IconColor.DEFAULT}
+          />
+        )}
         <input
           type="file"
           ref={inputRef}
           style={{ display: "none" }}
-          value={value ? value : ""}
+          defaultValue={value ? value : ""}
+          onChange={(e) => {
+            handleImageInput(e);
+          }}
         />
       </div>
     </div>
